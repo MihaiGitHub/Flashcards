883c11fe75a50f45381a9bd5af3900959a4dc85f
Michael S <mihai.sanfran@gmail.com>
2018-12-09 20:13:37 -0700

    Redux boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+// Communication glue between redux and react
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 const App = () => {
     return (
-        <View>
-            <Text>My App!</Text>
-        </View>
+        /* Provider tag is communication with React */
+        <Provider store={createStore}>
+            <View>
+                <Text>My App!</Text>
+            </View>
+        </Provider>
     );
 };
 

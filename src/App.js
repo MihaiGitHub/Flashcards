380ec8898850762868313e815eafd75410c44240
Michael S <mihai.sanfran@gmail.com>
2018-12-09 20:29:47 -0700

    Create libraries reducer and pass to store

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { View, Text } from 'react-native';
 // Communication glue between redux and react
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 const App = () => {
     return (
-        /* Provider tag is communication with React */
-        <Provider store={createStore}>
+        /* Provider tag is communication with React;
+        Create store with reducers and pass to Provider tag */
+        <Provider store={createStore(reducers)}>
             <View>
                 <Text>My App!</Text>
             </View>

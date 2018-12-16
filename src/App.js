6522fba7c6e32a922ced400e386e664243f92eab
Michael S <mihai.sanfran@gmail.com>
2018-12-15 18:41:45 -0700

    Import Header component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
     return (
@@ -11,6 +12,8 @@ const App = () => {
         Create store with reducers and pass to Provider tag */
         <Provider store={createStore(reducers)}>
             <View>
+                <Header headerText="Tech Stack" />
+            
                 <Text>My App!</Text>
             </View>
         </Provider>

f76122343824fdf333666ab2712477c2c24484f7
Michael S <mihai.sanfran@gmail.com>
2018-12-15 21:04:41 -0700

    Create LibraryReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
 
 export default combineReducers({
-    // libraries reducer always returns an array
-    libraries: () => []
+    // libraries reducer always returns an array; 
+    // whenever this reducer runs it will return a list of libraries to the user
+    // LibraryReducer assigned to the key libraries in state
+    // When console.log(store.getState()) = { libraries: [] }
+    libraries: LibraryReducer
 });

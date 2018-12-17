8595b3155b49ddffe79ae386f0343a13b1efb43a
Michael S <mihai.sanfran@gmail.com>
2018-12-16 20:11:41 -0700

    Handle selectLibrary action inside SelectionReducer

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,4 +1,11 @@
-export default () => {
-    // Always need to return something
-    return null;
+export default (state = null, action) => {
+    // Will return the actions payload (library id)
+    console.log(action);
+
+    switch(action.type){
+        case 'select_library':
+            return action.payload;
+        default:
+            return state;
+    }
 };
